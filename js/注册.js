0db87cc6b8233b1048c05a4270aded9bdae50643
Xunmi <xunmi1@outlook.com>
2017-10-20 18:20:23 +0800

    增加注册

--- js/注册.js
+++ js/注册.js
@@ -1,3 +1,4 @@
+
 $("form :input").blur(function() {       /*表单选择器，返回form中的所有表单对象,添加失去焦点事件*/
     var $parent = $(this).parent();
     var msg = "";
@@ -5,13 +6,37 @@ $("form :input").blur(function() {       /*表单选择器，返回form中的所
     $parent.find(".msg").remove();
 
     /*用户名验证*/
-    /*this属于DOM对象，$(this)属于JQuery对象*/
+    /*this属于DOM对象，$(this)属于jQuery对象*/
     if ($(this).is("#name")) {
         if (this.value == "" || this.value.length < 6) {
             msg = "用户名至少为6位！";
             $parent.append("<span id='false' class='msg'>" + msg + "</span>");
         } else {
-            msg = "用户名可以使用！";
+            msg = "用户名可用";
+            $parent.append("<span id='true' class='msg'>" + msg + "</span>");
+        }
+    }
+
+    /*手机号验证*/
+    if ($(this).is("#phone")) {
+        var regphone = /^1[34578]\d{9}$/;
+        if (!regphone.test(this.value)) {
+            msg = "手机号错误!";
+            $parent.append("<span id='false' class='msg'>" + msg + "</span>");
+        } else {
+            msg = "手机号可用";
+            $parent.append("<span id='true' class='msg'>" + msg + "</span>");
+        }
+    }
+
+    /*邮箱验证*/
+    if ($(this).is("#email")) {
+        var regemail = /^\w+[-\.\w]*@[a-zA-Z0-9]+([-\.][a-zA-Z0-9]+)*\.[a-zA-Z0-9]+$/;
+        if (!regemail.test(this.value)) {
+            msg = "邮箱错误!";
+            $parent.append("<span id='false' class='msg'>" + msg + "</span>");
+        } else {
+            msg = "邮箱正确";
             $parent.append("<span id='true' class='msg'>" + msg + "</span>");
         }
     }
@@ -26,9 +51,9 @@ $("form :input").blur(function() {       /*表单选择器，返回form中的所
             $parent.append("<span id='true' class='msg'>" + msg + "</span>");
         }
     }
-}).keyup(function() {
-    $(this).triggerHandler("blur");
 }).focus(function() {
+    $(this).triggerHandler("blur");
+}).keyup(function() {
     $(this).triggerHandler("blur");
 });
 
